d59b9195de15c3fa67bf175b9981036484064d7a
h-w-park <muyhye@gmail.com>
2020-01-16 14:23:48 +0900

    default

---
+++ js/ui.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('.doctor-like').on('click',function(){
+        $(this).toggleClass('on');
+    })
+    var $winHei = $('body').height();
+    $('.dimm').css('height',$winHei+'px');
+    $('.popup-close').on('click',function(){
+        $(this).closest('.popup').hide();
+        $('.dimm').hide();
+    });
+
+	$('.doctor').slick({
+		
+        dots: true,
+        centerMode: true,
+		centerPadding: '-320px',
+        slidesToScroll: 5,
+        infinite: true,
+        variableWidth: true
+	});
+
+	$('.slide-logo').slick({
+		//centerMode: true,
+		centerPadding: '120px',
+  variableWidth: true,
+		  infinite: true,
+		  variableWidth: true,
+		  autoplay: true,
+		  autoplaySpeed: 2000
+	});
+
+	$(window).on('scroll',function(){
+        var scrX = $(window).scrollLeft()
+        $('header').css('left','-'+scrX+'px')
+    });
+});
