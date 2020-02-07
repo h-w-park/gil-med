f79e76d94f70204181b4983aa7b80b899f6a7fa3
h-w-park <muyhye@gmail.com>
2020-02-07 10:32:09 +0900

    진료협력센터 지도

--- center/js/ui.js
+++ center/js/ui.js
@@ -1,3 +1,49 @@
+
+
+function maps(){
+
+	var img = $("#level1");
+	var img2 = $(".level2 figure");  
+
+	$("#maps area[href^='#']").click(function(e) {
+		e.preventDefault();
+		//왼쪽 맵 변경하기
+		var href = $(this).attr("href");
+		var map = href.substring(href.lastIndexOf("#")+1);
+		//seoul, busan ...
+		img.attr("src", "../../img/map/"+map+".png");		
+		
+		//오른쪽 맵 변경하기
+		$(img2).each(function() {   
+			$(this).removeClass("active");
+			$(this).css("z-index", "0");           
+			if ($(this).hasClass(map)) {   
+				$(this).addClass("active")
+				$(this).css("z-index", "2");       
+			}  
+		});
+	});  
+
+	$(".mapSm area[href^='#']").click(function(e) {
+		e.preventDefault();
+		var href22 = $(this).attr("href");
+		var map22 = href22.substring(href22.lastIndexOf("#")+1);
+		console.log(map22);
+		var aktiv = $("figure.active");
+		var href = $(aktiv).attr("class");
+		console.log(href);
+		var map = href.substring(0, href.length - 7);
+		console.log(map);
+
+		$(aktiv).each(function() {         
+			$('img', $(this)).attr("src", "../../img/map/"+map+"/"+map22+".png");	
+		});
+	});  
+
+}
+
+
+
 $(document).ready(function() {
     $('.doctor-like').on('click',function(){
         $(this).toggleClass('on');
