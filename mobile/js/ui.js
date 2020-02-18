5a39118810a9ce4072655f4225d410e6dc921826
Hye <hyewon@hyewon.local>
2020-02-18 21:37:24 +0900

    모바일 추가

--- mobile/js/ui.js
+++ mobile/js/ui.js
@@ -8,9 +8,7 @@ $(document).ready(function() {
 		//yearSuffix: "년",
 		monthNamesShort: [ "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월" ],
 		dayNamesMin: [ "일", "월", "화", "수", "목", "금", "토" ]
-
 	});
-
 
     $(".doctor-like").on('click',function(){
         $(this).toggleClass('on');
